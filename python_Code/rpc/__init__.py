d56ffe3b816958cb0f5b5d70bab98b4a89b57a18
John-Tekle <75260658+John-Tekle@users.noreply.github.com>
2023-11-29 16:00:32 +0100

    Smart_Pong_RPC has been implemented

--- python_Code/rpc/__init__.py
+++ python_Code/rpc/__init__.py
@@ -1,8 +1,26 @@
 from simple_rpc import Interface
 
-# interface = Interface('COM4')
 
-class Test():
-    
-    def test():
-        print("Hello..")
+class Smart_Pong_RPC():
+
+    def __init__(self, port='', baudrate=230400) -> None:
+        try:
+            self.interface = Interface(port, baudrate)
+        except Exception as e:
+            print(e)
+
+    def get_cup_A(self) -> bool:
+        try:
+            if self.interface.get_sensor1():
+                return True
+            return False
+        except Exception as e:
+            print(e)
+
+    def get_cup_B(self) -> bool:
+        try:
+            if self.interface.get_sensor2():
+                return True
+            return False
+        except Exception as e:
+            print(e)
