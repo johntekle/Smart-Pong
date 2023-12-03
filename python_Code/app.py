8dc1a8361a20bde09422c2be02f7ce473cb7e17d
John-Tekle <75260658+John-Tekle@users.noreply.github.com>
2023-12-03 16:25:45 +0100

    Button request was added

--- python_Code/app.py
+++ python_Code/app.py
@@ -16,27 +16,29 @@ smart_rpc = Smart_Pong_RPC("COM11")
 time.sleep(3)
 
 def poll_sensors():
-    global cupA_detected, cupB_detected
+    global cupA_detected, cupB_detected, button_detected
 
     while True:
         try:
             if smart_rpc.get_cup_A():
                 if not cupA_detected:
                     cupA_detected = True
-                time.sleep(0.05)
+                # time.sleep(0.05)
             else:
                 if cupA_detected:
                     cupA_detected = False
-                time.sleep(0.05)
+                # time.sleep(0.05)
 
             if smart_rpc.get_cup_B():
                 if not cupB_detected:
                     cupB_detected = True
-                time.sleep(0.05)
+                # time.sleep(0.05)
             else:
                 if cupB_detected:
                     cupB_detected = False
-                time.sleep(0.05)
+                # time.sleep(0.05)
+
+            button_detected = smart_rpc.get_button()
 
         except KeyboardInterrupt as e:
             print("\nProgramm is interrupted!\n")
@@ -49,6 +51,7 @@ def index():
 @app.route('/get_status')
 def get_status():
     global cupA_detected, cupB_detected, button_detected
+    # print(button_detected)
     return {'cup_A_status': cupA_detected, 'cup_B_status': cupB_detected, 'button_status': button_detected}
 
 if __name__ == '__main__':
